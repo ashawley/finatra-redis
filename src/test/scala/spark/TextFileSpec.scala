271d04435ae13bf19a9c0551bb237f0ed52a2c4f
Aaron S. Hawley <aaron.s.hawley@gmail.com>
2016-05-02 22:56:43 -0400

    Find line using Spark's lookup method

--- src/test/scala/spark/TextFileSpec.scala
+++ src/test/scala/spark/TextFileSpec.scala
@@ -3,6 +3,7 @@ package spark
 
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
 
 import org.scalacheck.Properties
 import org.scalacheck.Prop
@@ -12,19 +13,25 @@ class TextFileSpec extends Properties("spark.TextFile")
     with generator.JavaFile
     with generator.RandomFile {
 
-  val sparkConf = new SparkConf().
-    setAppName("spark.TextFileSpec")
-    .setMaster("local")
-    .set("spark.driver.allowMultipleContexts", "true")
-    .set("spark.akka.timeout", "60")
-
-  val sc = new SparkContext(sparkConf)
-
-  property("read") = {
+  property("count") = {
     Prop.forAll { (f: java.io.File, randFile: random.File) =>
       randFile.write(f)
-      val nLines = sc.textFile(f.getCanonicalPath).count
+      val nLines = SparkLocal.sc.textFile(f.getCanonicalPath).count
       (randFile.lineMin <= nLines && nLines <= randFile.lineMax)
+    }
+  }
+
+  property("lookup(x)") = {
+    Prop.forAll { (f: java.io.File, randFile: random.File) =>
+      randFile.write(f)
+      val lines: RDD[String] = SparkLocal.sc.textFile(f.getCanonicalPath)
+      val lineIdx: RDD[(Long, String)] = lines.zipWithIndex.map {
+        case (t, k) => (k, t)
+      }
+      val x = randFile.randNumLine
+      val line: String = lineIdx.lookup(x - 1).head
+      val lineLength: Int = line.length
+      (randFile.colMin <= lineLength && lineLength <= randFile.colMax)
     }
   }
 }
