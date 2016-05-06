d01ede768c41be716706f719aafe36b376fe541b
Aaron S. Hawley <aaron.s.hawley@gmail.com>
2016-05-06 18:04:01 -0400

    Add error handling to ./run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+if [ -z "${1}" ]; then
+    echo "No file specified" >&2
+    echo -e "Usage:\n\t./run.sh FILE" >&2
+    exit 1
+elif [ ! -f "${1}" ]; then
+    echo "File ${1} does not exist"
+    exit 1
+fi
+
+echo Starting server...
+echo ^C to exit
+
 set -x
 
-sbt "run -file='${1}'"
+sbt "run -file=${1}"
