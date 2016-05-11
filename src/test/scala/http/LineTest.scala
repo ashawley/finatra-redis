244e450230a2bf18963a94dc087514439b826fbe
Aaron S. Hawley <aaron.s.hawley@gmail.com>
2016-05-11 09:33:44 -0400

    Add integration test for 200 response

--- src/test/scala/http/LineTest.scala
+++ src/test/scala/http/LineTest.scala
@@ -11,6 +11,11 @@ import com.twitter.finatra.http.test.EmbeddedHttpServer
 import com.twitter.finagle.http.Status
 import com.google.inject.testing.fieldbinder.Bind
 
+import com.twitter.util.Future
+
+import com.twitter.util.Await
+import com.twitter.conversions.time._
+
 class LineTest extends FeatureTest with Mockito {
 
   override val server = new EmbeddedHttpServer(
@@ -27,9 +32,19 @@ class LineTest extends FeatureTest with Mockito {
     }
   }
 
-  "/line/1" should {
-    "Ok" in {
+  "/line/0" should {
+    "413" in {
+      textFile.line(anyLong).returns(Future(None))
+
       server.httpGet("/line/1", andExpect = Status(413))
+    }
+  }
+
+  "/line/1" should {
+    "200" in {
+      textFile.line(anyLong).returns(Future(Some("x")))
+
+      server.httpGet("/line/1", andExpect = Status(200))
     }
   }
 }
